873422c19e13a109a0a77645485ef648a8b2ecca
Lawrence Borst <laurens.s.borst@gmail.com>
2025-01-14 00:21:46 +0100

    Add input field component
    This input field allows the user to enter text
    to communicate with the  bot

--- frontend/french-bot/src/components/body/input-field.tsx
+++ frontend/french-bot/src/components/body/input-field.tsx
@@ -8,8 +8,40 @@ export class AppInputField {
   render() {
     return (
       <div>
-        <h1>Input Field</h1>
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="text">First name:</label>
+          <input type="text" id="text" name="text" />
+          <br />
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
+  }
+
+  private onSubmit = async (event: SubmitEvent): Promise<void> => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const targetEl: HTMLFormElement = event.target as HTMLFormElement;
+    const inputEl: HTMLInputElement = targetEl.querySelector('#text');
+    const text: string = inputEl.value;
+    inputEl.value = '';
+
+    await this.makeRequest(text);
+  };
+
+  private async makeRequest(text: string): Promise<string | void> {
+    const url = `http://127.0.0.1:5000/process-input?text=${text}`;
+
+    return fetch(url)
+      .then((response: Response) => {
+        if (!response.ok) {
+          throw new Error(`Response not OK: returned status code ${response.status}`);
+        }
+        return response.json();
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
 }
