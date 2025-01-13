3c7ebf12289e9f841ba00f428c6e484dfa439b7c
Lawrence Borst <laurens.s.borst@gail.com>
2025-01-14 00:21:29 +0100

    Add process-input endpoint
    This endpoint is a dummy endpoint, intended to process
    user input to the bot

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route("/")
 def index():
     return "<h1>Hello World</h1>"
+
+
+@app.route("/process-input", methods=["GET"])
+def process_input():
+    text: str = request.args.get("text")
+    if not text:
+        return 400
+
+    return jsonify("Hello World")
 
 
 if __name__ == "__main__":
