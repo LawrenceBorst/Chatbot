0d8c0b39a13c8fad442b389ecbda9dbb0382c4e8
Lawrence Borst <laurens.s.borst@gmail.com>
2025-02-22 21:36:38 +0100

    Add conversations endpoint

--- backend/app/main/views.py
+++ backend/app/main/views.py
@@ -1,6 +1,7 @@
 from flask import request, jsonify
 from . import main
-from flask_login import login_required
+from flask_login import login_required, current_user
+from ..models import Conversation
 
 
 @main.route("/process-input", methods=["GET"])
@@ -15,3 +16,21 @@ def process_input():
         return 400
 
     return jsonify("Hello World")
+
+
+@main.route("/conversations", methods=["GET"])
+@login_required
+def conversations():
+    """
+    This endpoint fetches a list of conversations for a given user
+    """
+    conversations = Conversation.query.filter_by(owner=current_user.id).all()
+
+    return [
+        {
+            "name": conversation.name,
+            "id": conversation.id,
+            "timestamp": conversation.timestamp.isoformat(),
+        }
+        for conversation in conversations
+    ]
